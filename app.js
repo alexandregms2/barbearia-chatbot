e189489f9be914a5cd9767c41a5a4c77bb084c91
alexandregms2 <alexandregms2@gmail.com>
2025-04-12 00:34:33 -0300

    changed app.js to fix build in vercel

--- app.js
+++ app.js
@@ -8,19 +8,30 @@ const admin = require('firebase-admin');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Configuração do Firebase
-// PARA TESTE LOCAL USAR A LINHA ABAIXO
-// const serviceAccount = require('./barbearia-app-alexandre-firebase-admin.json');
-
-// Carregar variáveis de ambiente do arquivo .env em ambiente de desenvolvimento
+// Configuração de variáveis de ambiente para ambiente local
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// Configuração do Firebase usando variáveis de ambiente
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+};
+
 // Inicializar Firebase
+// Inicialização do Firebase
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  projectId: "barbearia-app-alexandre"
+  credential: admin.credential.cert(serviceAccount)
 });
 
 const db = admin.firestore();
